6faef259eb57c23a2ad5a69db1b825ac15958c19
Fabian2023 <fasalpe34@gmail.com>
2025-04-07 20:48:50 -0500

    feat(version2): seccion 4 in progress 2

--- src/components/sections/vrOculus.jsx
+++ src/components/sections/vrOculus.jsx
@@ -5,7 +5,7 @@ export const VrOculus = () => {
 
   return (
     <section id="VR-Oculus" className="w-screen h-screen">
-      <div className="flex flex-col md:flex-row w-full h-screen">
+      <div className="flex flex-col-reverse md:flex-row w-full h-screen">
         {!showAlternateView ? (
           <div className="flex flex-col md:h-screen w-full md:w-2/3 justify-center px-6 md:pl-90 pl-12 md:pr-29 pr-12 pt-40 md:pt-20 gap-16 md:gap-10">
             <h2 className="md:text-5xl text-5xl font-bold text-left">
@@ -38,20 +38,14 @@ export const VrOculus = () => {
         ) : (
           <div className="flex flex-col md:h-screen w-full md:w-2/3 justify-center px-6 md:pl-90 pl-12 md:pr-29 pr-12 pt-40 md:pt-20 gap-16 md:gap-10">
             <h2 className="md:text-4xl text-5xl font-bold text-left">
-              Beneficios de Oculus <br/>
+              Beneficios de Oculus <br />
               para tu Marca
             </h2>
             <p className="md:text-xl text-3xl text-left">
-              <span className="font-semibold">
-                Inmersion Total:
-              </span>{" "}
-              Captura la atencion de tu publico con<br />experiencias que lo trasnportan a otra realidad
+              <span className="font-semibold">Inmersion Total:</span>{" "}
+              Captura la atencion de tu publico con<br />
+              experiencias que lo trasnportan a otra realidad
             </p>
-          
-        
-
-           
-
             <div className="flex gap-6 mt-6">
               <button className="bg-[rgba(92,41,112,1)] text-white rounded-full px-6 py-4 text-2xl font-bold w-72">
                 Explora el Futuro
@@ -65,11 +59,11 @@ export const VrOculus = () => {
             </div>
           </div>
         )}
-        <div className="md:w-1/2 w-full h-screen flex-shrink-0 flex md:items-center items-center justify-center">
+        <div className="md:w-1/2 w-full h-screen flex-shrink-0 flex items-center justify-center">
           <img
             src="/oculus.png"
             alt="Oculus VR"
-            className="max-w-full max-h-full pl-20 object-contain"
+            className="max-w-full max-h-full object-contain"
           />
         </div>
       </div>
