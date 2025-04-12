4ff3678b607ce7af6fa63f7721eddf106e249b76
Juan Camilo Arenas <j.arenas@elysgame.com>
2025-04-12 08:26:41 -0500

    seccion tres 80 %

--- src/components/sections/nuestrosServicios.jsx
+++ src/components/sections/nuestrosServicios.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination } from 'swiper/modules';
+import { Pagination } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
@@ -45,46 +45,64 @@ const servicios = [
 ];
 
 export const NuestrosServicios = () => {
-    return (
-      <section id="Nuestros-Servicios" className="w-screen h-screen ">
-        <div className="flex flex-col  space-y-8 h-screen w-full md:pt-43 gap-30 ">
-          <h2 className="text-5xl font-bold text-center text-left">
-            Servicios que Ofrecemos
-          </h2>
-            <div className='flex justify-end w-full'>
-                          <Swiper
-            modules={[Navigation, Pagination]}
-            spaceBetween={1}
-            slidesPerView={5}
-            breakpoints={{
-              640: { slidesPerView: 2 },
-              768: { slidesPerView: 2 },
-              1024: { slidesPerView: 3 },
-            }}
-            navigation
-            pagination={{ clickable: true }}
-            className="w-2/3 ml-auto"
-          >
-            {servicios.map((servicio, index) => (
-              <SwiperSlide key={index}>
-                <div
-                  className={`w-[200px] h-[310px]  rounded-2xl p-6 text-white bg-gradient-to-br ${servicio.color} shadow-lg hover:scale-105 transition-transform duration-300 `}
-                >
-                  <h3 className="text-2xl font-semibold mb-2">
-                    {servicio.nombre}
-                  </h3>
-                  <p className="mb-4 text-sm">{servicio.descripcion}</p>
-                  {/* <button className="bg-white text-black px-4 py-2 rounded-lg font-semibold hover:bg-gray-200 transition">
-                    {servicio.boton}
-                  </button> */}
-                </div>
-              </SwiperSlide>
-            ))}
-          </Swiper>
-            </div>
+  const swiperRef = useRef(null);
 
+  return (
+    <section id="Nuestros-Servicios" className="w-screen h-screen">
+      <div className="flex flex-col space-y-8 h-screen w-full pt-43 gap-30 md:gap-10">
+        <h2 className="text-5xl font-bold text-center md:pl-80 text-left pl-[10%]">
+          Servicios que <br/> Ofrecemos
+        </h2>
+
+        <div className="flex flex-col items-center w-full h-full gap-15">
+          <div className="w-full  md:pl-80 pl-[10%]">
+            <Swiper
+              modules={[Pagination]}
+              onSwiper={(swiper) => (swiperRef.current = swiper)}
+              spaceBetween={1.2}
+              slidesPerView={1.2}
+              breakpoints={{
+                640: { slidesPerView: 1.2 },
+                768: { slidesPerView: 2.2 },
+                1024: { slidesPerView: 5.2 },
+              }}
+              // pagination={{ clickable: true }}
+              className="ml-auto"
+            >
+              {servicios.map((servicio, index) => (
+                <SwiperSlide key={index}>
+                  <div
+                    className={`tt h-[450px] w-[270px] md:w-[200px] md:h-[310px]  rounded-2xl p-10 text-white bg-gradient-to-br ${servicio.color} shadow-lg hover:scale-105 transition-transform duration-300 justify-end flex flex-col items-center gap-10 md:gap-2 pb-10 md:pb-4`}
+                  >
+                    <h3 className="w-full text-4xl font-semibold text-left pb-10 ">
+                      {servicio.nombre}
+                    </h3>
+                    <p className="text-3xl md:text-lg text-left pr-8 md:pr-9">{servicio.descripcion}</p>
+                    <button className='w-full mx-auto  h-1/8 text-white bg-[#753E89] text-2xl font-semibold rounded-full hover:bg-purple-800 transition'>¡Quiero innovar!</button>
+                  </div>
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </div>
+
+          {/* Flechas debajo del carrusel */}
+          <div className="flex md:justify-end justify-center gap-10 mt-6 w-full md:pr-35">
+          <button
+            onClick={() => swiperRef.current?.slidePrev()}
+            className="w-25 h-25 md:h-15 md:w-15 bg-gray-300 rounded-full hover:bg-gray-400 transition text-lg font-bold flex items-center justify-center"
+          >
+            ←
+          </button>
+          <button
+            onClick={() => swiperRef.current?.slideNext()}
+            className="w-25 h-25 md:h-15 md:w-15 bg-gray-300 rounded-full hover:bg-gray-400 transition text-lg font-bold flex items-center justify-center"
+          >
+            →
+          </button>
+
+          </div>
         </div>
-      </section>
-    );
-  };
-  
+      </div>
+    </section>
+  );
+};
