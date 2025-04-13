727b13ec17b4cfb6a51062a58ce4fe4878543110
Juan Camilo Arenas <j.arenas@elysgame.com>
2025-04-12 21:23:45 -0500

    arreglos

--- src/components/sections/nuestrosServicios.jsx
+++ src/components/sections/nuestrosServicios.jsx
@@ -5,69 +5,8 @@ import { Link } from 'react-router-dom';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
+import { servicios } from '../../utilities/servicesJson/servicesJson';
 
-const servicios = [
-  {
-    id: 'photobot',
-    nombre: 'Photobot',
-    descripcion: 'Ofrecemos publicidad interactiva con anuncios dinámicos que captan la atención y generan mayor impacto.',
-    color: 'from-purple-800 to-fuchsia-600',
-  },
-  {
-    id: 'trivia',
-    nombre: 'Trivia',
-    descripcion: 'Ofrecemos publicidad interactiva con anuncios dinámicos que captan la atención y generan mayor impacto.',
-    color: 'from-blue-800 to-indigo-600',
-  },
-  {
-    id: 'speak-ia',
-    nombre: 'Speak IA',
-    descripcion: 'Ofrecemos publicidad interactiva con anuncios dinámicos que captan la atención y generan mayor impacto.',
-    color: 'from-gray-700 to-slate-500',
-  },
-  {
-    id: 'desarrollos',
-    nombre: 'Desarrollos personalizados',
-    descripcion: 'Ofrecemos publicidad interactiva con anuncios dinámicos que captan la atención y generan mayor impacto.',
-    color: 'from-yellow-900 to-orange-600',
-  },
-  {
-    id: 'ruleta',
-    nombre: 'Ruleta',
-    descripcion: 'Ofrecemos publicidad interactiva con anuncios dinámicos que captan la atención y generan mayor impacto.',
-    color: 'from-red-700 to-pink-600',
-  },
-  {
-    id: 'rompecabezas',
-    nombre: 'Rompecabezas',
-    descripcion: 'Ofrecemos publicidad interactiva con anuncios dinámicos que captan la atención y generan mayor impacto.',
-    color: 'from-purple-900 to-purple-600',
-  },
-  {
-    id: 'IA',
-    nombre: 'Talk IA',
-    descripcion: 'Ofrecemos publicidad interactiva con anuncios dinámicos que captan la atención y generan mayor impacto.',
-    color: 'from-red-900 to-purple-600',
-  },
-  {
-    id: 'registro',
-    nombre: 'Punto de registro',
-    descripcion: 'Ofrecemos publicidad interactiva con anuncios dinámicos que captan la atención y generan mayor impacto.',
-    color: 'from-green-900 to-blue-600',
-  },
-  {
-    id: 'registro',
-    nombre: 'Catalogo de Productos',
-    descripcion: 'Ofrecemos publicidad interactiva con anuncios dinámicos que captan la atención y generan mayor impacto.',
-    color: 'from-yellow-900 to-red-600',
-  },
-  {
-    id: 'rompecabezas',
-    nombre: 'Rompecabezas',
-    descripcion: 'Ofrecemos publicidad interactiva con anuncios dinámicos que captan la atención y generan mayor impacto.',
-    color: 'from-pink-900 to-gray-600',
-  },
-];
 
 export const NuestrosServicios = () => {
   const swiperRef = useRef(null);
