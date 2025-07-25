a9b65f45ca78c222ef70e6c91eea9587034ec631
Juan Camilo Arenas <jc.arenas1993@gmail.com>
2025-07-25 11:12:37 -0500

    fix

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,7 +5,6 @@ import {  HomeNavBar } from "../components/sections/homeNavbar";
 import { InnovacionInteractiva } from "../components/sections/innovacionInteractiva";
 import { NuestrosServicios} from "../components/sections/nuestrosServicios";
 import {  VrOculus } from "../components/sections/vrOculus";
-import { CasosDeExito } from "../components/sections/CasosDeExito";
 import { PorQueElegirnos } from "../components/sections/porQueElegirnos";
 import { Contactanos } from "../components/sections/contactanos";
 import { SeccionesUnidas } from "../components/sections/seccionesUnidas";
